0cdf580a8b0ccd7e575fd4982f9b25b61a502516
JuwonLee99 <joofuud98@gmail.com>
2022-12-19 16:48:35 +0900

    함수 퀴즈 답안 작성

--- Quiz_6.py
+++ Quiz_6.py
@@ -6,20 +6,36 @@
 # 조건1 : 표준체중은 별도의 함수 내에서 계산
 #         * 함수명 : std_weight
 #         * 전달값 : 키(height), 성별(gender)
-# 조건2 : 표준 체중은 소수점 둘째자리까지 표시
+# 조건2 : 표준 체중은 소수점 둘째자리까지 표시  - round(숫자, 반올림할 소수 자리)
 
 
 # (출력 예제)
 # 키 175cm 남자의 표준 체중은 67.38kg 입니다.
 
 
-def std_weight(height, gender) :
+#[작성 답안]
+
+# def std_weight(height, gender) :
+#     if gender == '남자' :
+#         print('키 {0}cm 남자의 표준 체중은 {1}kg 입니다.' .format(height, round(pow(height*0.01, 2)*22,2)))
+#         return height, round(pow(height*0.01, 2)*22, 2)
+#     elif gender == '여자' :
+#         print('키 {0}cm 여자의 표준 체중은 {1}kg 입니다.' .format(height, round(pow(height*0.01, 2)*21, 2)))
+#         return height, round(pow(height*0.01, 2)*21, 2)
+
+
+# std_weight(175, '남자')
+
+
+#[답안]
+def std_weight (height, gender) : 
     if gender == '남자' :
-        print('키 {0}cm 남자의 표준 체중은 {1}kg 입니다.' .format(height, round(pow(height*0.01, 2)*22,2)))
-        return height, round(pow(height*0.01, 2)*22, 2)
-    elif gender == '여자' :
-        print('키 {0}cm 여자의 표준 체중은 {1}kg 입니다.' .format(height, round(pow(height*0.01, 2)*21, 2)))
-        return height, round(pow(height*0.01, 2)*21, 2)
+        return height * height * 22 
+    else :
+        return height * height * 21
 
+height = 175
+gender = '남자'
+weight = round(std_weight(height/100, gender), 2)
+print('키 {}cm {}의 표준 체중은 {}kg 입니다.' .format(height, gender, weight))
 
-std_weight(175, '남자')
