416a6546c50c773030184b798502a4ce37539bca
JuwonLee99 <joofuud98@gmail.com>
2022-12-28 16:23:10 +0900

    no message

--- 함수_키워드값.py
+++ 함수_키워드값.py
@@ -1,5 +1,8 @@
-def profile (name, age, main_lang) :
-    print(name, age, main_lang)
+# def profile (name, age, main_lang) :
+#     print(name, age, main_lang)
 
-profile(name='유재석', main_lang='파이썬', age = '20')
-profile(main_lang = '자바', age = 25, name='이광수')
+# profile(name='유재석', main_lang='파이썬', age = '20')
+# profile(main_lang = '자바', age = 25, name='이광수')
+
+
+# 함수에서 전달받는 키워드에 따라 값을 주면 순서가 뒤바뀌어있어도 순서대로 전달됨
